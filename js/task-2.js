925ed712f239771d3337108fba1b0195d5b9896c
Kulik Kateryna <katerynakulik4@gmail.com>
2025-06-10 23:02:34 +0300

    3 tasks

---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+class Storage {
+   
+    constructor(items){
+        this.items=items;
+    };
+    getItems(){
+        return this.items;
+    };
+    addItem(newItem){
+        return this.items.push(newItem);
+    };
+    removeItem(itemToRemove){
+        if (this.items.includes(itemToRemove)){
+        const a = this.items.indexOf(itemToRemove);
+        return this.items.splice(a, 1);
+        };
+    };
+};
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
